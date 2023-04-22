694094e76f192e52b28dba69c25378900e603f9a
Nataliia_Rud <nrud1981@gmail.com>
2023-04-22 20:14:17 +0300

    update

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ContactList } from 'components/ContactList/ContactList';
+import { FilterContainer, FilterInput } from './Filter.styled';
+export const Filter = ({ onInput }) => {
+  {
+    return (
+      <FilterContainer>
+        <p>Find contacts by name</p>
+        <FilterInput
+          type="text"
+          name="filter"
+          //   value={aaa}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={onInput}
+        />
+      </FilterContainer>
+    );
+  }
+};
+
+Filter.propTypes = {
+  onInput: PropTypes.func.isRequired,
+};
